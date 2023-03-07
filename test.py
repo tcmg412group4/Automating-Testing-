bde507d0fda7c4a9efaa0c2eaf07319d07568e42
Austin John <austinjohn@tamu.edu>
2023-03-06 23:15:29 -0600

    -0

--- test.py
+++ test.py
@@ -8,59 +8,43 @@ BASE = "http://0.0.0.0:4000"                                  #local host:port d
 
 response = requests.get(BASE + "/md5/Hello World")              #sends URI to API
 data = response.json()                                          #sets data as the json output
-print(data)                                                     #prints output
+                                                   #prints output
 
 #############################
 # Factorial test
 
 response = requests.get(BASE + "/factorial/4")
 data = response.json()
-print(data)
-
-
 
 response = requests.get(BASE + "/factorial/0")
 data = response.json()
-print(data)
-
 
 #############################
 # Is-prime test
 
 response = requests.get(BASE + "/is-prime/10")
 data = response.json()
-print(data)
-
 
 response = requests.get(BASE + "/is-prime/7")
 data = response.json()
-print(data)
-
 
 response = requests.get(BASE + "/is-prime/1")
 data = response.json()
-print(data)
 
 #############################
 # Fibonacci test
 
 response = requests.get(BASE + "/fibonacci/20")
 data = response.json()
-print(data)
-
 
 response = requests.get(BASE + "/fibonacci/100")
 data = response.json()
-print(data)
-
 
 response = requests.get(BASE + "/fibonacci/0")
 data = response.json()
-print(data)
 
 #############################
-# Slack-Alert Test
+##### Slack-Alert Test
 
 response = requests.get(BASE + "/slack-alert/2nd Test")
 data = response.json()
-print(data)
